8fbf2a17b3c0716b377a355784b16b93c55e8991
his1devil <evilat@outlook.com>
2016-10-15 23:30:45 +0800

    乱序字符串

---
+++ anagramgroup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+"""
+乱序字符串
+给出一个字符串数组S，找到其中所有的乱序字符串
+如果一个字符串是乱序字符串，那么存在一个字母集合相同
+但顺序不同的字符串也在S中
+"""
+
+class Solution(object):
+    def anagrams(self, strs):
+
