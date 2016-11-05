c3916c78172a2878fe65e95738375b23256e320c
his1devil <evilat@outlook.com>
2016-11-06 00:03:23 +0800

    Continued

---
+++ crosslinkedlist.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def node(self, node1, node2):
+        m, n = len()
